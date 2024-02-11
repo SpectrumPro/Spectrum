8238f58ff1c085cc59044dc66e3e08b337b3bf27
Liam Sherwin <something.random.i.made.at.3am@gmail.com>
2024-02-11 14:04:49 +1300

    Refactored tab script

--- Scripts/Tab_container.gd
+++ Scripts/Tab_container.gd
@@ -1,37 +1,33 @@
 # WIP
 extends TabContainer
 
-var last_click_time := 0
-var double_click_threshold := 0.5  # Adjust this as needed
+var last_click_time : int = 0
+var double_click_threshold : float = 0.5  # Adjust this as needed
 
-func _ready():
-	#set_tab_button_icon(get_current_tab(), load("res://Assets/Icons/close.svg"))
-	pass
-func _on_tab_clicked(tab_index):
-	#if not get_current_tab() == get_previous_tab():
-		#set_tab_button_icon(get_current_tab(), load("res://Assets/Icons/close.svg"))
-		#set_tab_button_icon(get_previous_tab(), Texture2D.new())
-	#pass
-	var current_time = Time.get_ticks_msec() / 1000.0
+func _ready() -> void:
+	set_tab_button_icon(get_current_tab(), load("res://Assets/Icons/close.svg"))
+
+func _on_tab_clicked(tab_index:int) -> void:
+	if not get_current_tab() == get_previous_tab():
+		set_tab_button_icon(get_current_tab(), load("res://Assets/Icons/close.svg"))
+		set_tab_button_icon(get_previous_tab(), Texture2D.new())
 	
+	var current_time : float = Time.get_ticks_msec() / 1000.0
 	if current_time - last_click_time < double_click_threshold:
-		# Double click detected
 		on_double_click(tab_index)
 	else:
-		# Single click, update last click time
 		last_click_time = current_time
 
 	
-func on_double_click(tab_index):
-
+func on_double_click(tab_index:int) -> void:
 	create_new_window(tab_index)
 
-func create_new_window(tab_index):
-	var node_to_replace = get_tab_control(tab_index)
-	var new_window_node = Window.new()
+func create_new_window(tab_index:int) -> void:
+	var node_to_replace : Control = get_tab_control(tab_index)
+	var new_window_node : Window = Window.new()
 	
 	new_window_node.name = node_to_replace.name
-	new_window_node.close_requested.connect(self.window_close_request.bind(str(node_to_replace.name)))
+	new_window_node.close_requested.connect(self.window_close_request.bind(new_window_node))
 	new_window_node.set_initial_position(Window.WINDOW_INITIAL_POSITION_CENTER_SCREEN_WITH_KEYBOARD_FOCUS)
 	new_window_node.size = DisplayServer.window_get_size()
 	
@@ -44,16 +40,15 @@ func create_new_window(tab_index):
 	node_to_replace.replace_by(new_window_node)
 	node_to_replace.queue_free()
 
-func window_close_request(node_name):
-	var node_to_replace = get_node(node_name)
+func window_close_request(node:Window) -> void:
 	var new_control_node = Control.new()
 	
-	new_control_node.name = node_name
+	new_control_node.name = node.name
 	
-	node_to_replace.remove_child(node_to_replace.get_node("_window_backdrop"))
-	node_to_replace.replace_by(new_control_node)
-	node_to_replace.queue_free()
+	node.remove_child(node.get_node("_window_backdrop"))
+	node.replace_by(new_control_node)
+	node.queue_free()
 
-func _on_tab_button_pressed(tab):
+func _on_tab_button_pressed(tab:int) -> void:
 	if tab == get_current_tab():
-		set_tab_hidden(tab, true)
+		get_tab_control(tab).queue_free()
