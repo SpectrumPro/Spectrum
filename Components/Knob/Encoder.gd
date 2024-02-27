d02f83016dd07d0911a8f10f3770dff9a2ad6dfe
Liam Sherwin <something.random.i.made.at.3am@gmail.com>
2024-02-27 21:05:56 +1300

    Refactored Virtual Fixtures Panel (and more icons))

---
+++ Components/Knob/Encoder.gd
@@ -0,0 +1,95 @@
+extends Control
+## Simple encoder knob
+
+signal value_changed(value:int) ## Emitted when the value is changed
+signal movement_started ## Emitted when the user begins moving the knob
+signal movement_ended ## Emitted when the user finishes moving the knob
+
+@export var value: int = 0: set = set_value, get = get_value ## Value of the encoder
+@export var rotation_offset: int = 0 ## Changes where the mid point of min and max value is located on the knob, value is in degrees
+@export var min_value: int = 0
+@export var max_value: int = 100
+@export var sensitivity: float = 0.5
+@export var angle_gap: int = 60 ## The gap in degress between [param min_value] and [param max_value] on the encoder, allowing for a visual gap between values
+@export var snapping_increment: int = 10 ## Snapping distance, used when holding the control key
+@export var wrap_around_value: bool = false ## If true the encoder will spin for ever, and the value will wrap to the opposite when it hits an end
+
+var _start_mouse_pos: Vector2
+var _current_rotation: int
+var _value: int = value
+
+var _gap_min: int = 0 + angle_gap
+var _gap_max: int = 360 - angle_gap
+
+func _ready() -> void:
+	## Disable process, so it is only enabled when the user is interacting with the knob
+	
+	_gap_min = 0 + angle_gap
+	_gap_max = 360 - angle_gap
+	
+	self.set_process(false)
+	($TextureRect as TextureRect).pivot_offset = size / 2
+	set_value(value)
+	
+	
+
+func set_value(value: int) -> int:
+	## Set the value of the Encoder, emits value_changed signal
+	var new_value: int  = set_value_no_signal(value)
+	
+	value_changed.emit(new_value)
+	
+	return new_value
+
+
+func set_value_no_signal(value: int) -> int:
+	## Sets the value of the encoder, does not emit a signal
+	
+	var new_value: int = value
+	var rotation: int = 0
+	
+	if wrap_around_value:
+		rotation = wrapi(remap(value, min_value, max_value, 0, 360), 0, 360)
+		new_value = wrapi(new_value, min_value, max_value)
+	else:
+		rotation = clampi(remap(value, min_value, max_value, _gap_min, _gap_max), _gap_min, _gap_max)
+		new_value = clampi(value, min_value, max_value)
+	
+	print(rotation)
+	
+	$TextureRect.rotation_degrees = rotation + rotation_offset
+	$Label.text = str(new_value)
+	
+	_value = new_value
+	return new_value
+
+
+func get_value() -> int:
+	return _value
+
+
+func _on_texture_rect_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			_start_mouse_pos = self.get_global_mouse_position()
+			_current_rotation = $TextureRect.rotation_degrees - rotation_offset
+			self.set_process(true)
+
+
+func _process(_delta) -> void:
+	var distance: Vector2 = _start_mouse_pos - self.get_global_mouse_position()
+	var new_value: int = _current_rotation + ((distance.x * -1) + (distance.y * -1)) * sensitivity
+	
+	new_value = remap(new_value, _gap_min, _gap_max, min_value, max_value)
+	
+	if Input.is_key_pressed(KEY_CTRL):
+		new_value = snappedi(new_value, snapping_increment)
+	
+	value = new_value
+	
+	if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		self.set_process(false)
+
+
+func _on_texture_rect_resized() -> void:
+	($TextureRect as TextureRect).pivot_offset = size / 2
---
+++ Panels/Virtual Fixtures/Virtual_fixtures.gd
@@ -0,0 +1,128 @@
+# Copyright (c) 2024 Liam Sherwin
+# All rights reserved.
+
+extends GraphEdit
+## UI panel for displaying virtual fixtures
+
+const ORIENTATION_VERTICAL: int = 0
+const ORIENTATION_HORIZONTAL: int = 1
+
+
+var _old_active_fixtures: Array
+var _selected_virtual_fixtures: Array
+
+var _add_fixture_button: Button
+
+var _position_offset: Vector2 = Vector2(100, 100)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	## Add extra buttons to GraphEdit menu, and subscribe to global variables
+	
+	_add_fixture_button = _add_menu_hbox_button("Add Selected Fixture", self._add_fixture, "Add the selected fixtures to the view", true)
+	_add_menu_hbox_button("Delete", self._request_delete, "Delete the selected virtual fixtures, this does NOT delete the underlying fixture")
+	_add_menu_hbox_button(ResourceLoader.load("res://Assets/Icons/Horizontal_distribute.svg"), self._align.bind(ORIENTATION_HORIZONTAL), "Align the selected fixtures horizontally" )
+	_add_menu_hbox_button(ResourceLoader.load("res://Assets/Icons/Vertical_distribute.svg"), self._align.bind(ORIENTATION_VERTICAL), "Align the selected fixtures verticality" )
+	
+	Globals.subscribe("active_fixtures", self._active_fixtures_changed)
+
+
+func _add_menu_hbox_button(content:Variant, method: Callable, tooltip: String = "", disabled: bool = false) -> Button:
+	## Function to add a button to the Graph Edits menu HBox, with callbacks, tool tips, and shortcuts
+	
+	var button: Button = Button.new()
+	
+	if content is Texture2D:
+		button.icon = content
+	else:
+		button.text = content as String
+	
+	button.pressed.connect(method)
+	button.tooltip_text = tooltip
+	button.disabled = disabled
+	
+	self.get_menu_hbox().add_child(button)
+	return button
+
+
+func _add_fixture() -> void:
+	## Adds the currently selected virtual fixtures to the view
+	
+	for fixture: Fixture in Globals.get_value("active_fixtures"):
+		var node_to_add: Control = Globals.components.virtual_fixture.instantiate()
+		
+		fixture.add_virtual_fixture(node_to_add)
+		
+		node_to_add.control_node = fixture
+		node_to_add.set_highlighted(true)
+		node_to_add.position_offset += _position_offset
+		_position_offset += Vector2(5, 5)
+		
+		self.add_child(node_to_add)
+
+
+func _request_delete() -> void:
+	## Deletes a virtual fixtures from the current view
+	
+	var to_remove: Array = _selected_virtual_fixtures.duplicate()
+	
+	for virtual_fixture: Control in to_remove:
+		virtual_fixture.control_node.remove_virtual_fixture(virtual_fixture)
+		virtual_fixture.queue_free()
+		_selected_virtual_fixtures.erase(virtual_fixture)
+
+  
+func _active_fixtures_changed(new_active_fixtures: Array) -> void:
+	## Function to update highlighting on virtual fixtures, when their corresponding fixture is selected
+	
+	_add_fixture_button.disabled = true if new_active_fixtures == [] else false
+	
+	for virtual_fixture: Control in get_children():
+		virtual_fixture.set_highlighted(false)
+	
+	for active_fixture: Fixture in new_active_fixtures:
+		for virtual_fixture in active_fixture.virtual_fixtures:
+			virtual_fixture.set_highlighted(true)
+	
+	_old_active_fixtures = new_active_fixtures
+
+
+func _align(orientation: int) -> void:
+	## Function to align the currently selected fixtures horizontally
+	
+	if not _selected_virtual_fixtures:
+		return
+		
+	var base_position: Vector2 = _selected_virtual_fixtures[0].position_offset
+	var i: int = 0
+	
+	for virtual_fixture: Control in _selected_virtual_fixtures:
+		virtual_fixture.position_offset = base_position
+		
+		if orientation:
+			base_position.x += 100
+		else:
+			base_position.y += 100
+
+
+func from(config: Dictionary, control_fixture: Fixture) -> void:
+	## Function to load a virtual fixture, from a stored config in a save file
+	
+	var node_to_add: Control = Globals.components.virtual_fixture.instantiate()
+	
+	node_to_add._position_offset = Vector2(config._position_offset.x, config._position_offset.y)
+	node_to_add.control_node = control_fixture
+	control_fixture.add_virtual_fixture(node_to_add)
+	
+	self.add_child(node_to_add)
+
+
+func _on_virtual_fixture_selected(node) -> void:
+	if node not in _selected_virtual_fixtures:
+		_selected_virtual_fixtures.append(node)
+	Globals.select_fixture(node.control_node)
+
+
+func _on_virtual_fixture_deselected(node) -> void:
+	_selected_virtual_fixtures.erase(node)
+	Globals.deselect_fixture(node.control_node)
