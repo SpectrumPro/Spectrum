e10020bc2aec770e2ccb176d3b78f8e881ed368a
Liam Sherwin <something.random.i.made.at.3am@gmail.com>
2024-04-09 18:51:16 +1200

    Removed auto load

--- Scripts/Menu_buttons.gd
+++ Scripts/Menu_buttons.gd
@@ -2,7 +2,7 @@ extends HBoxContainer
 
 
 func _ready():
-	Core.load(OS.get_environment("HOME") + "/Documents/Spectrum/Test1.spshow")
+	#Core.load(OS.get_environment("HOME") + "/Documents/Spectrum/Test1.spshow")
 	pass
 
 func _on_save_pressed() -> void:
