515f3b04490feaabca9a943e40b6d1f8d3ae802b
Liam <liambrackensherwin@gmail.com>
2024-07-29 16:40:56 +1200

    Ui Improvements

--- components/ChannelSlider/ChannelSlider.gd
+++ components/ChannelSlider/ChannelSlider.gd
@@ -5,33 +5,132 @@
 class_name ChannelSlider extends PanelContainer
 ## The slider used for channel overrides
 
+
+## Emitted when the value is changed
+signal value_changed(value: int)
+
+
+## The label text
+@export var label_text: String = "Slider" : set = set_label_text
+
+## Wether to show the override warning background
+@export var show_warning_bg: bool = true : set = set_show_warning_bg
+
+## The slider value
+@export_range(0, 255) var value: int = 0 : set = set_value
+
+
+@export_group("Graident")
+
+## Wether to show the gradient background
+@export var show_gradient_bg: bool = false : set = set_show_graident_bg
+
+## Top and Bottom colors of the graident
+@export var graident_top_color: Color = Color.WHITE : set = set_graident_top_color
+@export var graident_bottom_color: Color = Color.BLACK : set = set_graident_bottom_color
+
+
+@export_group("Command")
+
+## Wether or not to send a command to the server
+@export var send_command: bool = true
+
+## The ID of the networked object
 @export var object_id: String = ""
+
+## The method to call
 @export var method: String = ""
+
+## The method to call when reseting
 @export var reset_method: String = ""
+
+
+
+@export_group("Arguments")
+
+## Args to send before the value
 @export var args_befour: Array = []
+
+## Args to send after the value
 @export var args_after: Array = []
-@export var label_text: String = "Slider": set = set_label_text
+
+## A selection value to send, "" to disable
 @export var send_selection_value: String = ""
 
 
-func set_label_text(text: String):
-	label_text = text
-	
-	if is_node_ready():
-		$VBoxContainer/Label.text = text
+## Nodes
+@onready var slider: VSlider = $MarginContainer/VBoxContainer/VSlider
+@onready var spin_box: SpinBox = $MarginContainer/VBoxContainer/SpinBox
+@onready var label: Label = $MarginContainer/VBoxContainer/LabelContainer/Label
 
-
-func clear_no_message() -> void:
-	$VBoxContainer/VSlider.set_value_no_signal(0)
-	$VBoxContainer/SpinBox.set_value_no_signal(0)
-	$WarningBG.hide()
 
 
 func _ready() -> void:
 	set_label_text(label_text)
+	set_show_warning_bg(show_warning_bg)
+	set_show_graident_bg(show_gradient_bg)
+	
+	set_graident_top_color(graident_top_color)
+	set_graident_bottom_color(graident_bottom_color)
+	
+	set_value(value)
+	
+	$GraidentContainer.visible = show_gradient_bg
+	
+	$GraidentContainer/GraidentBG.add_theme_stylebox_override("panel", $GraidentContainer/GraidentBG.get_theme_stylebox("panel").duplicate(true))
 
 
+## Sets the text in the main label
+func set_label_text(text: String):
+	label_text = text
+	if is_node_ready():  $MarginContainer/VBoxContainer/LabelContainer/Label.text = text
+
+
+## Sets wether to show the override warning background
+func set_show_warning_bg(state: bool) -> void:
+	show_warning_bg = state
+
+
+## Sets wether to show the gradient background
+func set_show_graident_bg(state: bool) -> void:
+	show_gradient_bg = state
+	if is_node_ready(): $GraidentContainer.visible = state
+
+
+## Sets the top color of the graident
+func set_graident_top_color(color: Color) -> void:
+	graident_top_color = color
+	if is_node_ready(): $GraidentContainer/GraidentBG.get_theme_stylebox("panel").texture.gradient.set_color(1, color)
+
+
+## Sets the bottom color of the graident
+func set_graident_bottom_color(color: Color) -> void:
+	graident_bottom_color = color
+	if is_node_ready(): $GraidentContainer/GraidentBG.get_theme_stylebox("panel").texture.gradient.set_color(0, color)
+
+
+## Sets the current value
+func set_value(p_value: int) -> void:
+	value = clamp(p_value, 0, 255)
+	
+	if is_node_ready():
+		slider.set_value_no_signal(p_value)
+		spin_box.set_value_no_signal(p_value)
+
+
+## Clears the value of the slider with out sending a message
+func clear_no_message() -> void:
+	value = 0
+	if show_warning_bg: $WarningBG.hide()
+	
+	value_changed.emit(0)
+
+
+## Sends the value message
 func _send_set_value_message(value: int) -> void:
+	
+	if not send_command: 
+		return
 	
 	var args: Array = []
 	
@@ -47,23 +146,37 @@ func _send_set_value_message(value: int) -> void:
 	})
 
 
-func _on_v_slider_value_changed(value: float) -> void:
-	$VBoxContainer/SpinBox.set_value_no_signal(value)
-	$WarningBG.show()
+## Called when the slider is moved
+func _on_v_slider_value_changed(p_value: float) -> void:
+	value = p_value
+	
+	if show_warning_bg: $WarningBG.show()
 	_send_set_value_message(value)
+	
+	value_changed.emit(value)
 
 
-func _on_spin_box_value_changed(value: float) -> void:
-	$VBoxContainer/VSlider.set_value_no_signal(value)
-	$WarningBG.show()
+## Called when the input box is changed
+func _on_spin_box_value_changed(p_value: float) -> void:
+	value = p_value
+	
+	if show_warning_bg: $WarningBG.show()
 	_send_set_value_message(value)
+	
+	value_changed.emit()
 
 
+## Called when the clear button is pressed
 func _on_clear_pressed() -> void:
 	clear_no_message()
 	
-	Client.send({
+	if send_command: Client.send({
 		"for": object_id,
 		"call": reset_method,
 		"args": args_befour if not send_selection_value else args_befour + [Values.get_selection_value(send_selection_value, [])]
 	})
+
+
+## Called when the random button is pressed
+func _on_random_pressed() -> void:
+	slider.value = randi_range(0, 255)
--- panels/Programmer/ColorSystem.gd
+++ panels/Programmer/ColorSystem.gd
@@ -7,13 +7,9 @@ extends HBoxContainer
 
 @export_node_path("ColorPicker") var color_picker: NodePath
 
-@export_node_path("VSlider") var red_slider: NodePath
-@export_node_path("VSlider") var green_slider: NodePath
-@export_node_path("VSlider") var blue_slider: NodePath
-
-@onready var red_slider_gradient: Gradient = get_node(red_slider).get_theme_stylebox("slider").texture.gradient
-@onready var green_slider_gradient: Gradient = get_node(green_slider).get_theme_stylebox("slider").texture.gradient
-@onready var blue_slider_gradient: Gradient = get_node(blue_slider).get_theme_stylebox("slider").texture.gradient
+@onready var red_slider: ChannelSlider = $ColorSliders/HBoxContainer/Red
+@onready var green_slider: ChannelSlider = $ColorSliders/HBoxContainer/Green
+@onready var blue_slider: ChannelSlider = $ColorSliders/HBoxContainer/Blue
 
 ## The current color of this color system
 var current_color: Color = Color.BLACK
@@ -23,14 +19,14 @@ var _last_call_time: int = 0
 
 ## Updates the background of the R G B sliders
 func update_slider_bg_colors():
-	red_slider_gradient.set_color(0, (current_color - Color.RED).clamp(Color.BLACK, Color.WHITE))
-	red_slider_gradient.set_color(1, (current_color + Color.RED).clamp(Color.BLACK, Color.WHITE))
+	red_slider.set_graident_bottom_color((current_color - Color.RED).clamp(Color.BLACK, Color.WHITE))
+	red_slider.set_graident_top_color((current_color + Color.RED).clamp(Color.BLACK, Color.WHITE))
 
-	green_slider_gradient.set_color(0, (current_color - Color.GREEN).clamp(Color.BLACK, Color.WHITE))
-	green_slider_gradient.set_color(1, (current_color + Color.GREEN).clamp(Color.BLACK, Color.WHITE))
+	green_slider.set_graident_bottom_color((current_color - Color.GREEN).clamp(Color.BLACK, Color.WHITE))
+	green_slider.set_graident_top_color((current_color + Color.GREEN).clamp(Color.BLACK, Color.WHITE))
 
-	blue_slider_gradient.set_color(0, (current_color - Color.BLUE).clamp(Color.BLACK, Color.WHITE))
-	blue_slider_gradient.set_color(1, (current_color + Color.BLUE).clamp(Color.BLACK, Color.WHITE))
+	blue_slider.set_graident_bottom_color((current_color - Color.BLUE).clamp(Color.BLACK, Color.WHITE))
+	blue_slider.set_graident_top_color((current_color + Color.BLUE).clamp(Color.BLACK, Color.WHITE))
 
 
 ## Called when the color picker is changed.
@@ -41,9 +37,9 @@ func _on_color_picker_color_changed(color: Color) -> void:
 		current_color = color
 		Core.programmer.set_color(Values.get_selection_value("selected_fixtures", []), current_color)
 		
-		get_node(red_slider).set_value_no_signal(current_color.r8)
-		get_node(green_slider).set_value_no_signal(current_color.g8)
-		get_node(blue_slider).set_value_no_signal(current_color.b8)
+		red_slider.set_value(current_color.r8)
+		green_slider.set_value(current_color.g8)
+		blue_slider.set_value(current_color.b8)
 		
 		update_slider_bg_colors()
 		
@@ -56,7 +52,6 @@ func _update_color(send_message: bool = true) -> void:
 	update_slider_bg_colors()
 	
 	if send_message: _send_to_programmer("set_color", current_color)
-	#Core.programmer.set_color(Values.get_selection_value("selected_fixtures", []), current_color)
 
 
 func _send_to_programmer(method_name: String, value: Variant = null) -> void:
@@ -80,15 +75,13 @@ func _on_green_slider_value_changed(value: float) -> void:
 func _on_blue_slider_value_changed(value: float) -> void:
 	current_color.b8 = value
 	_update_color()
-	
+
+
 func _on_color_reset_pressed() -> void: 
-	get_node(red_slider).set_value_no_signal(0)
-	get_node(green_slider).set_value_no_signal(0)
-	get_node(blue_slider).set_value_no_signal(0)
+	red_slider.set_value(0)
+	green_slider.set_value(0)
+	blue_slider.set_value(0)
 	
 	current_color = Color.BLACK
 	_update_color(false)
 	_send_to_programmer("reset_color")
-
-
-
